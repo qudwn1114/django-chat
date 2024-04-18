4e9cf7a6a42c8e37c10188a991b95f042dd40128
qudwn1114 <qudwn1114@gmail.com>
2024-04-18 16:11:25 +0900

    fix: settings.py

--- django-chat/config/settings.py
+++ django-chat/config/settings.py
@@ -134,8 +134,11 @@ USE_TZ = False
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
